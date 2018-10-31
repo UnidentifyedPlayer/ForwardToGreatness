c46693bfe2f66a5069a70ff32e1d1f43780d256b
Unidentifyed player <Zer0OOO>
2018-10-31 10:53:21 +0300

    1

---
+++ SmoothTheAngles/SmoothTheAngles/Class1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace SmoothTheAngles
+{
+    public class Polygon
+    {
+        public Bitmap bitmap;
+        public PointF[,] pts;
+        public MyPoint[] TheP;
+        public int Angles;
+        public float radius;
+        public Polygon(int ang, float rad, PointF[] pt, int w, int h)
+        {
+            TheP = new MyPoint[ang];
+            pts = new PointF[ang, 2];
+            for(int i = 0; i < ang; i++)
+            {
+                TheP[i] = new MyPoint(pt[i], rad);
+            }
+            Angles = ang;
+            radius = rad;
+            bitmap = new Bitmap(w, h);
+        }
+        private double ToRadians(double y0,double x0, double y1, double x1)
+        {
+            double k = (y0 - y1) / (x0 - x1);
+            return (Math.Atan(k) < 0) ? (2 * Math.PI - Math.Abs(Math.Atan(k))) : Math.Atan(k);
+        }
+        private double ToRadians(double y, double x)
+        {
+            double kk2 = y / x;
+            return (Math.Atan2(y,x) < 0) ? (2 * Math.PI - Math.Abs(Math.Atan2(y,x))) : Math.Atan2(y,x);
+        }
+        public void FindCoordinates(int i, float radius, PointF p1, PointF p2)
+        {
+
+            double dx1 = -(TheP[i].p.X - p1.X);
+            double dx2 = -(TheP[i].p.X - p2.X);
+            double dy1 = -(TheP[i].p.Y - p1.Y);
+            double dy2 = -(TheP[i].p.Y - p2.Y);
+
+            double kk1 = (dx1 == 0) ? 0 : dy1 / dx1;
+            double kk2 = (dx2 == 0) ? 0 : dy2 / dx2;
+            double tangi, angle;
+            double tangent;
+            double tt;
+            double fullangle;
+            //((kk1<0)?(Math.PI + Math.Atan(kk1)):(Math.Atan(kk1)))
+            //((kk2 < 0) ? (Math.PI + Math.Atan(kk2)) : (Math.Atan(kk2)))
+            double alk1 = Math.Atan(kk1);
+            double alk2 = Math.Atan(kk2);
+            double al1 = Math.Atan2(dy1, dx1);
+                double al2 = Math.Atan2(dy2, dx2);
+            double an1 = (kk1 == 0) ? ((dx1 == 0) ? (Math.PI / 2) : 0): ToRadians(dy1, dx1);
+                double an2 = (kk2 == 0) ? ((dx2 == 0) ? (Math.PI / 2) : 0) : ToRadians(dy2, dx2);
+            if ((kk1 == 0) && (kk2 == 0))
+            {
+                tangi = ((dx1 == 0) ? Math.Sign(dy1) : Math.Sign(dx1)) * ((dx2 == 0) ? Math.Sign(dy2) : Math.Sign(dx2));
+               angle = Math.Abs(Math.Atan(tangi));
+                fullangle = 0;
+                tangent = tangi;
+            }
+            else
+            {
+                fullangle = (Math.Abs(an1 - an2) > Math.PI) ? (2 * Math.PI - Math.Abs(an1 - an2)) : Math.Abs(an1 - an2);
+                tangi = Math.Tan(fullangle);
+                angle = fullangle / 2;
+                tt = Math.Atan(tangi);
+                // angle = (tangi<0)?((Math.PI - Math.Abs(Math.Atan(tangi)))/2):(Math.Atan(tangi)/2);
+                tangent = Math.Tan(Math.Atan(tangi) / 2);
+
+            }
+            double tangensu = Math.Tan(angle);
+            // double circleAngle = Math.PI - 
+            double tang = radius / Math.Abs(tangensu);
+            double line1 = FindLength(dx1, dy1);
+            double line2 = FindLength(dx2, dy2);
+            double length = Math.Min(line1, line2);
+
+            if (length < 2 * tang)
+            {
+                tang = length / 2;
+                radius = (float)(tang * Math.Abs(tangensu));
+            }
+            PointF pt1 = FindPoint1(TheP[i].p, tang, line1, -dx1, -dy1);
+            PointF pt2 = FindPoint2(TheP[i].p, tang, line2, -dx2, -dy2);
+            if (i == 0)
+            {
+                pts[Angles - 1, 1] = pt2;
+                pts[i, 0] = pt1;
+            }
+            else
+            {
+                pts[i, 0] = pt1;
+                pts[i - 1, 1] = pt2;
+            }
+                double trace = FindLength(tang, radius);
+            double k = ((kk1 == 0) && (kk2 == 0)) ? (tangi) : (Math.Tan((an1 +an2)/2));//((tangensu+kk1)/(1-kk1*tangensu)));
+            double b = TheP[i].p.Y - k * TheP[i].p.X;
+            double kr1 = (kk1 == 0) ? 0 : (-1 / kk1);
+            double kr2 = (kk2 == 0) ? 0 : (-1 / kk2);
+            PointF CircleCenter;
+            if ((dy1 == 0) & (Math.Round(kr1*1000000) == 0))
+            {
+                CircleCenter = new PointF(pt1.X, (float)(k * pt1.X + b));
+            }
+            else
+            {
+                double br1 = pt1.Y - kr1 * pt1.X;
+                CircleCenter = new PointF((float)((br1 - b) / (k - kr1)), (float)(k * (br1 - b) / (k - kr1) + b));
+            }
+            double AngleToBegin = ToRadians(pt1.Y - CircleCenter.Y, pt1.X - CircleCenter.X);
+            double AngleToEnd = ToRadians( pt2.Y - CircleCenter.Y, pt2.X - CircleCenter.X);
+            double TheCycle = AngleToEnd - AngleToBegin;
+
+            if (Math.Abs(TheCycle) > Math.PI)
+            {
+                TheCycle = -Math.Sign(TheCycle) * (-Math.Abs(TheCycle) + 2 * Math.PI);
+            }
+            if (kr2 != 0)
+            {
+                double ttt = Math.Tan(AngleToBegin + TheCycle);
+                double tttt = Math.Tan(AngleToBegin - TheCycle);
+                TheCycle = (Math.Round(Math.Tan(AngleToBegin + TheCycle), 2) == Math.Round(kr2, 2)) ? TheCycle : (-TheCycle);
+            }
+
+
+            TheP[i].TheCycle = TheCycle;
+            TheP[i].AngleToBegin = AngleToBegin;
+            TheP[i].radius = radius;
+            TheP[i].CircleCenter = CircleCenter;
+        }
+        public void Draw()
+        {
+            using (Graphics g = Graphics.FromImage(bitmap))
+            {
+                PointF[,] lineArgs = new PointF[Angles, 2];
+                g.Clear(Color.White);
+                for (int i = 0; i < TheP.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                       FindCoordinates(i, radius, TheP[i + 1].p, TheP[TheP.Length - 1].p);
+                    }
+                    else if (i == (TheP.Length - 1))
+                    {
+                        FindCoordinates(i, radius, TheP[0].p, TheP[i - 1].p);
+                    }
+                    else
+                    {
+                        FindCoordinates(i,radius, TheP[i + 1].p, TheP[i - 1].p);
+                    }
+                }
+                Pen muhPen = new Pen(Color.Black);
+                for (int i = 0; i < TheP.Length; i++)
+                {
+                    float xk = (int)pts[i, 0].X, yk = (int)pts[i, 0].Y;
+                    float k = Math.Max(Math.Abs((int)pts[i, 1].Y - (int)pts[i, 0].Y), Math.Abs((int)pts[i, 1].X - (int)pts[i, 0].X));
+                    for (int o = 0; o < k; o++)
+                    {
+                        g.DrawRectangle(muhPen, xk, yk, 1, 1);
+                        xk += ((int)pts[i, 1].X - (int)pts[i, 0].X) / k;
+                        yk += ((int)pts[i, 1].Y - (int)pts[i, 0].Y) / k;
+                    }
+                    //DrawDDA(muhPen, (int)pts[i, 0].X, (int)pts[i, 0].Y, (int)pts[i, 1].X, (int)pts[i, 1].Y);
+                    g.DrawArc(muhPen, TheP[i].CircleCenter.X - radius, TheP[i].CircleCenter.Y - TheP[i].radius, 2 * TheP[i].radius, 2 * TheP[i].radius, (float)((180 / Math.PI) * TheP[i].AngleToBegin), (float)((180 / Math.PI) * TheP[i].TheCycle));
+                    bitmap.SetPixel((int)TheP[i].CircleCenter.X, (int)TheP[i].CircleCenter.Y, Color.Red);
+                    bitmap.SetPixel((int)TheP[i].p.X, (int)TheP[i].p.Y, Color.CornflowerBlue);
+                    bitmap.SetPixel((int)TheP[i].p.X+1, (int)TheP[i].p.Y-1, Color.CornflowerBlue);
+                    bitmap.SetPixel((int)TheP[i].p.X+1, (int)TheP[i].p.Y+1, Color.CornflowerBlue);
+                    bitmap.SetPixel((int)TheP[i].p.X-1, (int)TheP[i].p.Y-1, Color.CornflowerBlue);
+                    bitmap.SetPixel((int)TheP[i].p.X-1, (int)TheP[i].p.Y+1, Color.CornflowerBlue);
+                }
+            }
+        }
+        private double FindLength(double dx, double dy)
+        {
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+        public void DrawDDA(Pen pen, int x1, int y1, int x2, int y2)
+        {
+            float xk = x1, yk = y1;
+            float k = Math.Max(Math.Abs(y2 - y1), Math.Abs(x2 - x1));
+            for (int i = 0; i < k; i++)
+            {
+                //this.g.DrawRectangle(pen, xk, yk, 1, 1);
+                xk += (x2 - x1) / k;
+                yk += (y2 - y1) / k;
+            }
+        }
+        public PointF FindPoint1(PointF p, double line1, double line2, double dx, double dy)
+        {
+            double ratio = line1 / line2;
+            return new PointF((float)(p.X - dx*ratio), (float)(p.Y - dy*ratio));
+        }
+        public PointF FindPoint2(PointF p, double line1, double line2, double dx, double dy)
+        {
+            double ratio = line1 / line2;
+            return new PointF((float)(p.X - dx * ratio), (float)(p.Y - dy * ratio));
+        }
+
+    }
+    public class MyPoint
+    {
+    public PointF p;
+        public double TheCycle;
+        public double AngleToBegin;
+        public PointF CircleCenter;
+        public float radius;
+        public MyPoint(PointF t, float rad)
+        {
+            p = t;
+            TheCycle = 0;
+            AngleToBegin = 0;
+            CircleCenter = new PointF(0, 0);
+            radius = rad; 
+        }
+    }
+}
---
+++ SmoothTheAngles/SmoothTheAngles/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SmoothTheAngles
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        PointF[] pointsArr;
+        int radius;
+        int counter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            g = pictureBox1.CreateGraphics();
+        }
+        private void DrawTheSmoothed(PointF P0, PointF p1, PointF p2, float radius, int i , ref PointF[,] lineArgs, int counter)
+        {
+            double dx1 = P0.X - p1.X;
+            double dx2 = P0.X - p2.X;
+            double dy1 = P0.Y - p1.Y;
+            double dy2 = P0.Y - p2.Y;
+
+            double theAngle = ( Math.Atan2(dy1, dx1) - Math.Atan2(dy2, dx2)) /2;
+            double tangensu = Math.Abs(Math.Tan(theAngle));
+            double smoothedLinePart1 = radius / tangensu;
+            double line10 = Math.Sqrt(dx1 * dx1 + dy1 * dy1);
+            double line02 = Math.Sqrt(dx2 * dx2 + dy2 * dy2);
+            double line = Math.Min(line10, line02);
+
+            if (smoothedLinePart1 > line)
+            {
+                smoothedLinePart1 = line;
+                radius = (float)(line / Math.Tan(theAngle));
+            }
+
+            PointF pointS1 = PointOfSmoothering(P0, smoothedLinePart1, line10, dx1, dy1);
+            PointF pointS2 = PointOfSmoothering(P0, smoothedLinePart1, line02, dx2, dy2);
+            if (i == 0)
+            {
+                lineArgs[counter - 1, 1] = pointS2;
+                lineArgs[i, 0] = pointS1;
+            }
+            else 
+            {
+                lineArgs[i, 0] = pointS1;
+                lineArgs[i - 1, 1] = pointS2;
+            }
+
+            double dx = (P0.X  - pointS1.X) + (P0.X - pointS2.X);
+            double dy = (P0.Y - pointS1.Y) + (P0.Y - pointS2.Y);
+
+            double L = FindLength(dx, dy);
+            double d = FindLength(smoothedLinePart1, radius);
+
+            PointF CircleCenter = PointOfSmoothering(P0, d, L, dx, dy);
+
+            double angleToBegin = Math.Atan2(pointS1.Y - CircleCenter.Y, pointS1.X - CircleCenter.X);
+            double angleToEnd = Math.Atan2( pointS2.Y - CircleCenter.Y, pointS2.X - CircleCenter.X);
+
+            double theCycle = angleToEnd - angleToBegin;
+            if (theCycle < 0)
+            {
+                angleToBegin = angleToEnd;
+                theCycle = -theCycle;
+            }
+            if (theCycle > Math.PI)
+            {
+                theCycle = Math.PI - theCycle;
+            }
+
+                Pen myPen = new Pen(Color.Black,2);
+
+            g.DrawArc(myPen, CircleCenter.X - radius, CircleCenter.Y - radius, 2 * radius, 2 * radius, (float)((180  / Math.PI) * angleToBegin), (float)((180 / Math.PI)* theCycle));
+
+        }
+        private PointF PointOfSmoothering(PointF p, double smoothedLinePart, double line,  double dx, double dy)
+        {
+            double diff = smoothedLinePart / line;
+            return new PointF((float)(p.X - dx * diff), (float)(p.Y - dy * diff));
+        }
+        private double FindLength(double dx, double dy)
+        {
+            return Math.Sqrt(dx * dx + dy * dy);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            counter = 0;
+            if(!Int32.TryParse(textBox1.Text,out radius)||!Int32.TryParse(textBox2.Text, out counter))
+            {
+                MessageBox.Show("Try putting the normal numbers once in a while(integral, that is).");
+            }
+            else
+            { 
+                pointsArr = new PointF[counter];
+                counter = 0;
+                g.Clear(Color.White);
+            }
+        }
+        public void DrawDDA(Pen pen, int x1, int y1, int x2, int y2)
+        {
+            float xk = x1, yk = y1;
+            float k = Math.Max(Math.Abs(y2 - y1), Math.Abs(x2 - x1));
+            for (int i = 0; i < k; i++)
+            {
+                this.g.DrawRectangle(pen, xk, yk, 1, 1);
+                xk += (x2 - x1) / k;
+                yk += (y2 - y1) / k;
+            }
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (counter < pointsArr.Length)
+            {
+                Pen muhPen = new Pen(Color.SteelBlue);
+                pointsArr[counter].X = e.X;
+                pointsArr[counter].Y = e.Y;
+                counter++;
+                DrawDDA(muhPen, e.X + 4, e.Y, e.X - 4, e.Y);
+                DrawDDA(muhPen, e.X, e.Y-4, e.X, e.Y+4);
+            }
+            else
+            {
+                MessageBox.Show("You're done with installing coordinates,\n consider activating the drawing process");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Polygon A = new Polygon(counter, radius, pointsArr,pictureBox1.ClientSize.Width, pictureBox1.ClientSize.Height);
+            A.Draw();
+            g.DrawImage(A.bitmap,pictureBox1.ClientRectangle);
+        }
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Text = "X = " + e.X + " Y = " + e.Y;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pointsArr = new PointF[] { new PointF(50, 50), new PointF(50, 100), new PointF(200, 100), new PointF(200, 50) };
+            counter = 4;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pointsArr = new PointF[] { new PointF(365, 327),  new PointF(159, 78), new PointF(338, 74), new PointF(156, 192) };
+            counter = 4;
+        }
+    }
+}
